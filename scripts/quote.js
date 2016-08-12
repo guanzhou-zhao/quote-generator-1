fc235a7ada2d7a7bf0308e12190a844f5894113a
Lisa <lisa.m.seeto@gmail.com>
2016-08-12 15:34:53 +1200

    use different api and clean up code

--- scripts/quote.js
+++ scripts/quote.js
@@ -2,36 +2,35 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
   document.getElementById('newQuoteBtn').addEventListener('click', makeNewRequest);
+  var httpRequest;
 }
 
 function makeNewRequest() {
-  var httpRequest = new XMLHttpRequest();
+  httpRequest = new XMLHttpRequest();
   if(!httpRequest) {
     console.log("failed to create request");
   } else {
     console.log("created a new request");
   }
-
-  httpRequest.open("GET", "https://theysaidso.p.mashape.com/quote?query=software", true);
-  httpRequest.setRequestHeader("X-Mashape-Key", "rqZM46yjxNmshV86eo7xauaMV71Yp1KAWsQjsnnc1zpOaAcZmi");
-
-  httpRequest.onload = function(e) {
-    if (httpRequest.readyState === 4) {
-     if (httpRequest.status === 200) {
-       getNewQuote(httpRequest.responseText);
-     } else {
-       console.error(httpRequest.statusText);
-     }
-   }
+  httpRequest.open("GET", "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", true);
+  httpRequest.onreadystatechange = checkRequestResponse;
+  httpRequest.send();
 }
 
-  httpRequest.send();
+function checkRequestResponse() {
+  if (httpRequest.readyState === 4) {
+    if (httpRequest.status === 200) {
+     getNewQuote(httpRequest.responseText);
+    } else {
+     console.error(httpRequest.statusText);
+    }
+  }
 }
 
 function getNewQuote(response) {
   console.log(response);
   var json = JSON.parse(response);
-  var quote = json.contents["quote"] + "<br/>" + "-" + json.contents["author"];
+  var quote = json.quoteText + "<br/>" + "-" + json.quoteAuthor;
 
   document.getElementById("quote").innerHTML=quote;
 
